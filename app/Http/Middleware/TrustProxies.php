0838afec4613bf1dc73c7cd7ee786508b861d4f1
Sahl Al-Obaid <sahelalobid1@gmail.com>
2026-01-14 11:39:27 +0300

    Adding Middleware File

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustProxies
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+}
